f8a94d40d29e81d6ccb996356cf0c968a7757526
tamasmeszaros <meszaros.q@gmail.com>
2021-02-23 16:17:52 +0100

    Add tests for BoundingCircle

--- tests/libslic3r/test_hollowing.cpp
+++ tests/libslic3r/test_hollowing.cpp
@@ -1,32 +1,54 @@
-#include <iostream>
-#include <fstream>
 #include <catch2/catch.hpp>
+#include "test_utils.hpp"
 
-#include <libslic3r/TriangleMesh.hpp>
 #include "libslic3r/SLA/Hollowing.hpp"
-#include <openvdb/tools/Filter.h>
-#include "libslic3r/Format/OBJ.hpp"
+//#include <openvdb/tools/Filter.h>
 
-#include <libnest2d/tools/benchmark.h>
 
-#include <libslic3r/SimplifyMesh.hpp>
+//#include <libnest2d/tools/benchmark.h>
 
-#if defined(WIN32) || defined(_WIN32)
-#define PATH_SEPARATOR R"(\)"
-#else
-#define PATH_SEPARATOR R"(/)"
-#endif
+//#include <libslic3r/SimplifyMesh.hpp>
+#include <libslic3r/BoundingCircle.hpp>
 
-static Slic3r::TriangleMesh load_model(const std::string &obj_filename)
-{
-    Slic3r::TriangleMesh mesh;
-    auto fpath = TEST_DATA_DIR PATH_SEPARATOR + obj_filename;
-    Slic3r::load_obj(fpath.c_str(), &mesh);
-    return mesh;
+TEST_CASE("Bounding circle for right angle triangle, Z: 0", "[BoundingCircle]") {
+    using namespace Slic3r;
+
+    std::array<Vec3f, 3> pts = {{
+        { .0f, .0f, .0f},
+        {10.f,  0.f, 0.f},
+        { 0.f, 5.f, 0.f}
+    }};
+
+    BoundingCircle bc{pts};
+
+    Vec3f refc = (pts[1] + pts[2]) / 2.f;
+
+    REQUIRE(bc.R == Approx((pts[1] + pts[2]).norm() / 2.));
+    REQUIRE(bc.center.x() == Approx(refc.x()));
+    REQUIRE(bc.center.y() == Approx(refc.y()));
+    REQUIRE(bc.center.z() == Approx(refc.z()));
 }
 
+TEST_CASE("Bounding circle for degenerate triangle", "[BoundingCircle]") {
+    using namespace Slic3r;
 
-TEST_CASE("Hollow two overlapping spheres") {
+    std::array<Vec3f, 3> pts = {{
+        { .0f, .0f, .0f},
+        {10.f,  0.f, 0.f},
+        { 0.f, 0.f, 0.f}
+    }};
+
+    BoundingCircle bc{pts};
+
+    Vec3f refc = (pts[1] + pts[2]) / 2.f;
+
+    REQUIRE(bc.R == Approx((pts[1] + pts[2]).norm() / 2.));
+    REQUIRE(bc.center.x() == Approx(refc.x()));
+    REQUIRE(bc.center.y() == Approx(refc.y()));
+    REQUIRE(bc.center.z() == Approx(refc.z()));
+}
+
+TEST_CASE("Hollow two overlapping spheres", "[Hollowing]") {
     using namespace Slic3r;
 
     TriangleMesh sphere1 = make_sphere(10., 2 * PI / 20.), sphere2 = sphere1;

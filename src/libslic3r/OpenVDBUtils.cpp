cwbullet/PrusaSlicer

commit 0c8764d90556d8df65d7e3d5d1ef573c282764e7

Eliminate memory leaks from hollowing code

src/libslic3r/OpenVDBUtils.cpp
@@ -62,6 +62,9 @@ openvdb::FloatGrid::Ptr mesh_to_grid(const TriangleMesh &            mesh,
                                  return !m->is_manifold() || m->volume() < EPSILON;
                              });
 
+    for (auto p = it; it != meshparts.end(); ++p)
+        delete *p;
+
     meshparts.erase(it, meshparts.end());
 
     openvdb::FloatGrid::Ptr grid;
@@ -69,6 +72,8 @@ openvdb::FloatGrid::Ptr mesh_to_grid(const TriangleMesh &            mesh,
         auto subgrid = openvdb::tools::meshToVolume<openvdb::FloatGrid>(
             TriangleMeshDataAdapter{*m, voxel_scale}, tr, exteriorBandWidth,
             interiorBandWidth, flags);
+
+        delete m;
 
         if (grid && subgrid) openvdb::tools::csgUnion(*grid, *subgrid);
         else if (subgrid) grid = std::move(subgrid);

commit e9a36fb5fa749d3c8e7a4f4af73b73fcbf97559d

Fix crash caused by 0c8764d90556d8df65d7e3d5d1ef573c282764e7

src/libslic3r/OpenVDBUtils.cpp
@@ -54,26 +54,23 @@ openvdb::FloatGrid::Ptr mesh_to_grid(const TriangleMesh &            mesh,
 {
     openvdb::initialize();
 
-    TriangleMeshPtrs meshparts = mesh.split();
+    TriangleMeshPtrs meshparts_raw = mesh.split();
+    auto meshparts = reserve_vector<std::unique_ptr<TriangleMesh>>(meshparts_raw.size());
+    for (auto *p : meshparts_raw)
+        meshparts.emplace_back(p);
 
-    auto it = std::remove_if(meshparts.begin(), meshparts.end(),
-                             [](TriangleMesh *m){
-                                 m->require_shared_vertices();
-                                 return !m->is_manifold() || m->volume() < EPSILON;
-                             });
-
-    for (auto p = it; it != meshparts.end(); ++p)
-        delete *p;
+    auto it = std::remove_if(meshparts.begin(), meshparts.end(), [](auto &m) {
+         m->require_shared_vertices();
+         return m->volume() < EPSILON;
+     });
 
     meshparts.erase(it, meshparts.end());
 
     openvdb::FloatGrid::Ptr grid;
-    for (TriangleMesh *m : meshparts) {
+    for (auto &m : meshparts) {
         auto subgrid = openvdb::tools::meshToVolume<openvdb::FloatGrid>(
             TriangleMeshDataAdapter{*m, voxel_scale}, tr, exteriorBandWidth,
             interiorBandWidth, flags);
-
-        delete m;
 
         if (grid && subgrid) openvdb::tools::csgUnion(*grid, *subgrid);
         else if (subgrid) grid = std::move(subgrid);